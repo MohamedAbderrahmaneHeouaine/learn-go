dd0564fd997c26d63a6de8647a7e2e085b2b66c2
MohamedAbderrahmaneHeouaine <m_heouaine@estin.dz>
2024-03-10 17:42:08 +0100

    var lesson var

--- var.go
+++ var.go
@@ -8,12 +8,22 @@ var (
 	var2 = ""
 )
 
+// to declare a lot of global constant
+const (
+	First = 1
+	Last  = 2
+)
+
 func main() {
 	//there are four ways to declare a variable
 	// these variable are local in the main function
-	var name = "Foo"
+	var name = "Foo" // infer string
 	var firstName string = "Foo"
 	var lastName string = "full"
 	fullName := firstName + lastName
+	//constant var
+	const version = 1
+	// we can use the first three-way to declare constant
+
 	fmt.Println(name, firstName, fullName)
 }
