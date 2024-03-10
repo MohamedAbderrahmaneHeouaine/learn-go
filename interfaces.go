9fddd0c5eaa01655f7502a81faf26b04c2bd6fba
MohamedAbderrahmaneHeouaine <m_heouaine@estin.dz>
2024-03-10 20:17:18 +0100

    interface lesson

---
+++ interfaces.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// interface
+type Storer interface {
+	GetAll() ([]int, error)
+	Put(int) error
+}
+
+type MongoDbStore struct {
+	// some value
+}
+
+func (mongoDbStore MongoDbStore) GetAll() ([]int, error) {
+	return []int{1, 3, 4, 5, 6}, nil
+}
+func (mongoDbStore MongoDbStore) Put(int) error {
+	return nil
+}
+
+type ApiServer struct {
+	store Storer
+}
+
+func main() {
+	apiServer := ApiServer{store: MongoDbStore{}}
+	numbers, error := apiServer.store.GetAll()
+	if error == nil {
+		fmt.Println(numbers)
+	}
+}
