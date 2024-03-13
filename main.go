ad528e3b6b067dbe4eba4c25168a929e702a5a38
MohamedAbderrahmaneHeouaine <m_heouaine@estin.dz>
2024-03-13 17:13:00 +0100

    more interface

--- main.go
+++ main.go
@@ -5,6 +5,38 @@ import (
 	"newproject/types"
 )
 
+type Putter interface {
+	Put(id int, val any) error
+}
+type Storage interface {
+	Putter
+	Get(id int) (any, error)
+}
+type FooStorage struct{}
+
+func (f *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+func (f *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
+type BarStorage struct{}
+
+func (f *BarStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+func (f *BarStorage) Put(id int, val any) error {
+	return nil
+}
+
+type Server struct {
+	store Storage
+}
+
+func updateValue(id int, val any, putter Putter) error {
+	return putter.Put(id, val)
+}
 func main() {
 	user := types.User{
 		Username: "aymen",
@@ -12,4 +44,14 @@ func main() {
 	}
 	fmt.Println("user")
 	fmt.Println(user)
+	s := &Server{
+		store: &FooStorage{},
+	}
+	//s.store.Get(1)
+	//s.store.Put(1, 3)
+	err := updateValue(1, 3, s.store)
+	if err != nil {
+		return
+	}
+	//fmt.Println(s.store.Put(1, 3))
 }
